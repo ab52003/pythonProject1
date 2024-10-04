86cef41cfde402711916bd700241e232982fecb8
ab52003 <ab52003@mail.ru>
2024-10-04 19:11:51 +0300

    corrections
    corrections for 2.4 and 2.5

--- my_work2.5.py
+++ my_work2.5.py
@@ -1,13 +1,13 @@
-def get_matrix(n, m, value):
-    matrix = []
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)
-    return matrix
-result1 = get_matrix(2, 3, 11)
-result2 = get_matrix(2, 5, 12)
-result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+def get_matrix(n, m, value): # объявление функции
+    matrix = [] # пустой список
+    for i in range(n): # внешний цикл
+        matrix.append([]) # добавление пустого списка
+        for j in range(m): # внутренний цикл
+            matrix[i].append(value) # добавление value в список
+    return matrix # возврат списка
+result1 = get_matrix(2, 3, 11) # 1 вариант списка
+result2 = get_matrix(2, 5, 12) # 2 вариант списка
+result3 = get_matrix(4, 2, 13) # 3 вариант списка
+print(result1) # вывод на экран 1 варианта списка
+print(result2) # вывод на экран 2 варианта списка
+print(result3) # вывод на экран 3 варианта списка
