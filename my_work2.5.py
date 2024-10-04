0371d14760be0cd4ac4394bebd73059b893db574
ab52003 <ab52003@mail.ru>
2024-10-04 18:56:40 +0300

    my_work2.5
    work 2.5

---
+++ my_work2.5.py
@@ -0,0 +1,13 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+result1 = get_matrix(2, 3, 11)
+result2 = get_matrix(2, 5, 12)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
