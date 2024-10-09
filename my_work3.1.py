1c75c3ea48922a7a38d16e9719ceff70ad9e83a1
ab52003 <ab52003@mail.ru>
2024-10-09 16:23:30 +0300

    my_work3.1
    3.1

--- my_work3.1.py
+++ my_work3.1.py
@@ -1,7 +1,24 @@
-calls = 0
-def count_calls()
-    calls = calls + 1
-def string_info()
+calls = 0 # счетчик
 
-def is_contains(,)
-    
+def count_calls(): # Функция count_calls
+    global calls # глобальная переменная
+    calls = calls + 1 # увеличение счетчика
+
+def string_info(string): # Функция string_info
+    print(len(string), string.upper(), string.lower()) # вывод на экран кортежа из: длины строки, строки в верхнем регистре, строки в нижнем регистре
+    count_calls() # вызов функции count_calls
+
+def is_contains(string, list_to_search): # Функция is_contains
+    string.lower() # перевод строки в нижний регистр
+    list_to_search = [x.lower() for x in list_to_search] # перевод списка в нижний регистр
+    print(string in list_to_search) # вывод на экран результат проверки наличия строки в списке
+    count_calls() # вызов функции count_calls
+
+while calls >= 0: # цикл вызова функций
+    string = input("Введите слово: ") # ввод строки
+    list_to_search = input("Введите список слов: ").split() # ввод списка
+    string_info(string) # вызов функции string_info
+    is_contains(string, list_to_search) # вызов функции is_contains
+    print(calls) # вывод счетчика
+    if input("Продолжить (да/нет): ") == 'нет': # условие продолжения цикла
+        break # выход из цикла
