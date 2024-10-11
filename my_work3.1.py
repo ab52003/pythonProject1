10fa5c4e9253d8d9ffa6b13e450db34765a5c071
ab52003 <ab52003@mail.ru>
2024-10-11 16:46:13 +0300

    Update my_work3.1
    3.1-1

--- my_work3.1.py
+++ my_work3.1.py
@@ -1,17 +1,21 @@
 calls = 0 # счетчик
+res = () # переменная
+res_1 = () # переменная
 
 def count_calls(): # Функция count_calls
     global calls # глобальная переменная
     calls = calls + 1 # увеличение счетчика
 
 def string_info(string): # Функция string_info
-    print(len(string), string.upper(), string.lower()) # вывод на экран кортежа из: длины строки, строки в верхнем регистре, строки в нижнем регистре
+    global res # глобальная переменная
+    res = (len(string), string.upper(), string.lower()) # кортежа из длины строки, строки в верхнем регистре, строки в нижнем регистре
     count_calls() # вызов функции count_calls
 
 def is_contains(string, list_to_search): # Функция is_contains
+    global res_1 # глобальная переменная
     string.lower() # перевод строки в нижний регистр
     list_to_search = [x.lower() for x in list_to_search] # перевод списка в нижний регистр
-    print(string in list_to_search) # вывод на экран результат проверки наличия строки в списке
+    res_1 = (string in list_to_search) # результат проверки наличия строки в списке
     count_calls() # вызов функции count_calls
 
 while calls >= 0: # цикл вызова функций
@@ -19,6 +23,8 @@ while calls >= 0: # цикл вызова функций
     list_to_search = input("Введите список слов: ").split() # ввод списка
     string_info(string) # вызов функции string_info
     is_contains(string, list_to_search) # вызов функции is_contains
-    print(calls) # вывод счетчика
-    if input("Продолжить (да/нет): ") == 'нет': # условие продолжения цикла
-        break # выход из цикла
+    print(res) # вывод на экран кортежа
+    print(res_1) # вывод на экран результата проверки наличия строки в списке
+    print(calls) # вывод на экран счетчика
+    if input("Продолжить (да/нет): ") == 'нет':  # условие продолжения цикла
+        break  # выход из цикла
