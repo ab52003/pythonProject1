ee087a1129af7c10dfe77a97accd5a540695f80c
ab52003 <ab52003@mail.ru>
2024-10-13 14:06:42 +0300

    my_work3.5
    3.5

---
+++ my_work3.5.py
@@ -0,0 +1,20 @@
+def get_multiplied_digits(n): # объявление функции
+    if n == 0: # если введен 0
+        return 0 # вернуть 0
+    elif any(ch.isdigit() for ch in str(n)) == False: # если не введено число
+        return "Число не введено" # вернуть сообщение
+    else: # иначе
+        str_number = str(int(str(n))) # удаление 0 из начала числа
+        if len(str_number) > 1: # если число больше одной цифры
+            for i in range(len(str_number)): # цикл проверки 0 в конце числа
+                if str_number[len(str_number) - 1] == '0': # если число оканчивается на 0
+                    str_number = str_number.rstrip('0') # удаление 0 в конце числа
+        first = int(str_number[0]) # первая цифра числа
+        if len(str_number) > 1: # если число больше одной цифры
+            return first * get_multiplied_digits(int(str_number[1:])) # вернуть произведение значащих цифр
+        else: # иначе
+            return first # вернуть одну цифру
+
+
+n = input('Введите число: ') # вввести число
+print(get_multiplied_digits(n)) # вызов функции
