896cca69a025f608c34b2ab1890eccafb024f527
ab52003 <ab52003@mail.ru>
2024-10-19 11:46:32 +0300

    updates

--- my_work3.6.py
+++ my_work3.6.py
@@ -1,45 +1,43 @@
 def calculate_structure_sum(data_structure):
     global result
     for i in data_structure:
-        if type(i) == str:
+        if isinstance(i, str):
             result += len(i)
-        elif type(i) == int:
+        elif isinstance(i, int):
             result += i
-        elif type(i) == list:
+        elif isinstance(i, list):
             for j in i:
-                if type(j) == str:
+                if isinstance(j, str):
                     result += len(j)
-                elif type(j) == int:
+                elif isinstance(j, int):
                     result += j
-                elif type(j) == list or type(j) == set or type(j) == dict or type(j) == tuple:
+                elif isinstance(j, list) or isinstance(j, set) or isinstance(j, dict) or isinstance(j, tuple):
                     calculate_structure_sum(j)
-        elif type(i) == dict:
+        elif isinstance(i, dict):
             for j in dict.keys(i):
-                if type(j) == str:
+                if isinstance(j, str):
                     result += len(j)
-                elif type(j) == int:
+                elif isinstance(j, int):
                     result += j
             for j in dict.values(i):
-                if type(j) == str:
+                if isinstance(j, str):
                     result += len(j)
-                elif type(j) == int:
+                elif isinstance(j, int):
                     result += j
-        elif type(i) == set:
+        elif isinstance(i, set):
             for j in i:
-                if type(j) == str:
+                if isinstance(j, str):
                     result += len(j)
-                elif type(j) == int:
+                elif isinstance(j, int):
                     result += j
-                elif type(j) == set or type(j) == list or type(j) == dict or type(j) == tuple:
+                elif isinstance(j, set) or isinstance(j, list) or isinstance(j, dict) or isinstance(j, tuple):
                     calculate_structure_sum(j)
-        elif type(i) == tuple:
+        elif isinstance(i, tuple):
             calculate_structure_sum(i)
     return result
 
 
 result = 0
-
-
 
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
