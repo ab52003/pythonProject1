8637e46a98dbbea2ce1b15325051cb3288182645
ab52003 <ab52003@mail.ru>
2024-11-25 17:50:10 +0300

    updates

--- my_work7.3.py
+++ my_work7.3.py
@@ -17,16 +17,13 @@ class WordsFinder:
 
     def get_all_words(self):
         all_words = {}
-        a = ()
         b = ()
         for i in range(len(self.args)):
             name = self.args[i]
             with open(name, encoding='utf-8') as file:
                 a = (str(name).lower())
-                for y in file:
-                    b = str(y).lower()
-                    b = re.sub(r'[^\w\s]', '', b).split()
-                    b = (b)
+                b = str(file.readlines()).lower()
+                b = re.sub(r'[^\w\s]', '', b).split()
                 all_words[a] = b
         return all_words
 
@@ -37,7 +34,7 @@ class WordsFinder:
         word = (str(word).lower())
         for name, words in a.items():
             if word in words:
-                f[name] = words.index(word)
+                f[name] = words.index(word) + 1
         return f
 
 
@@ -50,15 +47,15 @@ class WordsFinder:
                 f[name] = words.count(word)
         return f
 
-#w = WordsFinder('text_1.txt', 'text_2.txt', 'text_3.txt')
+w = WordsFinder('text_1.txt', 'text_2.txt', 'text_3.txt')
 
-#print(w.get_all_words())
+print(w.get_all_words())
 
-#print(w.get_file_names())
+print(w.get_file_names())
 
-#print(w.find('с'))
+print(w.find('с'))
 
-#print(w.count('с'))
+print(w.count('с'))
 
 w = WordsFinder('test.txt')
 
