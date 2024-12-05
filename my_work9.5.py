e0bbe529c949f8f2156a9fee4d5e4ba1b5416c4b
ab52003 <ab52003@mail.ru>
2024-12-05 15:27:34 +0300

    updates

--- my_work9.5.py
+++ my_work9.5.py
@@ -1,6 +1,9 @@
 class StepValueError(ValueError):
     pass
 
+class BorderValueError(ValueError):
+    pass
+
 class Iterator():
 
     def __init__(self, start, stop, step=1):
@@ -8,41 +11,46 @@ class Iterator():
         self.stop = stop
         self.step = step
         self.pointer = start
+        if (self.stop > 0 and self.step > 0) or (self.stop < 0 and self.step > 0):
+            self.stop +=1
+        elif (self.stop > 0 and self.step < 0) or (self.stop < 0 and self.step < 0):
+            self.stop -=1
         if self.step == 0:
             raise StepValueError('шаг не может быть равен 0')
+        elif (self.start > self.stop and self.step > 0) or (self.start < self.stop and self.step < 0):
+            raise BorderValueError('неверно заданы параметры диапазона')
 
     def __iter__(self):
         self.pointer = self.start
         return self
 
     def __next__(self):
-        if self.step > 0:
-            if (self.pointer + self.step) <= self.stop:
-                self.pointer += self.step
-                return self.pointer
-            else:
-                raise StopIteration()
-        elif self.step < 0:
-            if (self.pointer + self.step) >= self.stop:
-                self.pointer += self.step
-                return self.pointer
-            else:
-                raise StopIteration()
+        if (self.step > 0 and self.pointer >= self.stop) or (self.step < 0 and self.pointer <= self.stop):
+            raise StopIteration
+        current = self.pointer
+        self.pointer += self.step
+        return current
 
 
 try:
     iter1 = Iterator(100, 200, 0)
     for i in iter1:
-        print(i, end='')
+        print(i, end=' ')
 except StepValueError:
     print('Шаг указан неверно')
+except BorderValueError:
+    print('Границы указаны неверно')
+print()
 
 try:
     iter2 = Iterator(-5, 1)
     for i in iter2:
-        print(i, end=' ', sep = '\n')
+        print(i, end=' ')
 except StepValueError:
     print('Шаг указан неверно')
+except BorderValueError:
+    print('Границы указаны неверно')
+print()
 
 try:
     iter3 = Iterator(6, 15, 2)
@@ -50,19 +58,28 @@ try:
         print(i, end=' ')
 except StepValueError:
     print('Шаг указан неверно')
+except BorderValueError:
+    print('Границы указаны неверно')
+print()
 
-#try:
-    #iter4 = Iterator(5, 1, -1)
-    #for i in iter4:
-        #print(i, end=' ')
-#except StepValueError:
-    #print('Шаг указан неверно')
+try:
+    iter4 = Iterator(5, 1, -1)
+    for i in iter4:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+except BorderValueError:
+    print('Границы указаны неверно')
+print()
 
-#try:
-    #iter5 = Iterator(10, 1)
-    #for i in iter5:
-        #print(i, end=' ')
-#except StepValueError:
-    #print('Шаг указан неверно')
+try:
+    iter5 = Iterator(10, 1)
+    for i in iter5:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+except BorderValueError:
+    print('Границы указаны неверно')
+print()
 
 
