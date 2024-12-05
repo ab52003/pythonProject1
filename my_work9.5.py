ea86908317d6b4cdb9c5de4a138a2e3be560bdc0
ab52003 <ab52003@mail.ru>
2024-12-05 12:31:03 +0300

    updates

--- my_work9.5.py
+++ my_work9.5.py
@@ -1,7 +1,7 @@
 class StepValueError(ValueError):
     pass
 
-class Iterator:
+class Iterator():
 
     def __init__(self, start, stop, step=1):
         self.start = start
@@ -16,54 +16,53 @@ class Iterator:
         return self
 
     def __next__(self):
-        self.pointer += self.step
         if self.step > 0:
-            if self.pointer < self.stop:
+            if (self.pointer + self.step) <= self.stop:
                 self.pointer += self.step
                 return self.pointer
             else:
                 raise StopIteration()
         elif self.step < 0:
-            if self.pointer > self.stop:
+            if (self.pointer + self.step) >= self.stop:
                 self.pointer += self.step
                 return self.pointer
             else:
                 raise StopIteration()
 
 
-        try:
-            iter1 = Iterator(100, 200, 0)
-            for i in iter1:
-                print(i, end='')
-        except StepValueError:
-            print('Шаг указан неверно')
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end='')
+except StepValueError:
+    print('Шаг указан неверно')
 
-        #try:
-            #iter2 = Iterator(-5, 1)
-            #for i in iter2:
-                #print(i, end=' ')
-        #except StepValueError:
-            #print('Шаг указан неверно')
+try:
+    iter2 = Iterator(-5, 1)
+    for i in iter2:
+        print(i, end=' ', sep = '\n')
+except StepValueError:
+    print('Шаг указан неверно')
 
-        #try:
-            #iter3 = Iterator(6, 15, 2)
-            #for i in iter3:
-                #print(i, end=' ')
-        #except StepValueError:
-            #print('Шаг указан неверно')
+try:
+    iter3 = Iterator(6, 15, 2)
+    for i in iter3:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
 
-        #try:
-            #iter4 = Iterator(5, 1, -1)
-            #for i in iter4:
-                #print(i, end=' ')
-        #except StepValueError:
-            #print('Шаг указан неверно')
+#try:
+    #iter4 = Iterator(5, 1, -1)
+    #for i in iter4:
+        #print(i, end=' ')
+#except StepValueError:
+    #print('Шаг указан неверно')
 
-        #try:
-            #iter5 = Iterator(10, 1)
-            #for i in iter5:
-                #print(i, end=' ')
-        #except StepValueError:
-            #print('Шаг указан неверно')
+#try:
+    #iter5 = Iterator(10, 1)
+    #for i in iter5:
+        #print(i, end=' ')
+#except StepValueError:
+    #print('Шаг указан неверно')
 
 
