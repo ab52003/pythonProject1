a052154ceac5eb2065ce1f5c58a4176c59fd25a0
ab52003 <ab52003@mail.ru>
2024-12-12 17:04:27 +0300

    updates

---
+++ my_work10.2.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+class Knight(threading.Thread):
+    def __init__(self, name, power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        enemy = 100
+        day = 0
+        while enemy:
+            time.sleep(1)
+            enemy = enemy - self.power
+            day += 1
+            print(f'{self.name} сражается {day} дней (день, дня), осталось {enemy} воинов.')
+        print(f'{self.name} одержал победу спустя {day} дней (день, дня)!')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
+print('Все битвы закончились!')
