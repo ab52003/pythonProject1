58dbb0b4acc47c78251247b2b58562ca148f8a59
ab52003 <ab52003@mail.ru>
2024-12-23 15:49:13 +0300

    updates

--- My_work10.5.py
+++ My_work10.5.py
@@ -1,11 +1,9 @@
 import multiprocessing
 from multiprocessing import Pool
 import time
-import threading
 
-def read_info(*name):
+def read_info(name):
     all_data = []
-    name =  ''.join(name)
     with open(name, 'r', encoding='utf-8') as file:
         for line in file:
             data = file.readline()
@@ -14,28 +12,17 @@ def read_info(*name):
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
-start_time = time.perf_counter()
-for name in filenames:
-    thread = threading.Thread(target=read_info, args=name)
-    thread.start()
-end_time = time.perf_counter()
-elapsed_time = (end_time - start_time)
-print(f'Работа функции {elapsed_time}')
-
 #start_time = time.perf_counter()
-#if __name__ == '__main__':
-    #for name in filenames:
-        #process = multiprocessing.Process(target=read_info, args=name)
-        #process.start()
+#for name in filenames:
+    #linear = read_info(name)
 #end_time = time.perf_counter()
-#elapsed_time = end_time - start_time
+#elapsed_time = (end_time - start_time)
 #print(f'Работа функции {elapsed_time}')
 
-
-#start_time = time.perf_counter()
-#if __name__ == '__main__':
-    #with Pool(processes=5) as pool:
-        #res = pool.map(read_info, filenames)
-#end_time = time.perf_counter()
-#lapsed_time = end_time - start_time
-#print(f'Работа функции {elapsed_time}')
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    with Pool(processes=5) as pool:
+        res = pool.map(read_info, filenames)
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    print(f'Работа функции {elapsed_time}')
